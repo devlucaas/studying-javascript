27f46058fa4a4cec738466cf833630e02c80d00f
Lucas Santos <lucassq_@hotmail.com>
2023-03-25 11:25:50 -0300

    first commit

---
+++ Testes/script.js
@@ -0,0 +1,20 @@
+const defaultUser = {
+    name:'',
+    email:'',
+    level:1,
+}
+
+const user1 = {
+    ...defaultUser,
+    name: 'Bonieky',
+    email:'suport@b7web.com.br'
+}
+
+
+const amd1 = {
+    ...defaultUser,
+    name: 'admin boladao',
+    email:'admin@b7web.com.br',
+    level: 2,
+}
+
