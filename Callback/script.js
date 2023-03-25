5355be7dc66b10972d9db48a11eae7e6456761d0
Lucas Santos <lucassq_@hotmail.com>
2023-03-25 11:31:33 -0300

    editing callback

--- Callback/script.js
+++ Callback/script.js
@@ -4,5 +4,8 @@ document.querySelector('#botao')
     });
 
 /*
-Uma função callback é uma função passada como argumento para outra função, que será chamada de volta (ou executada) quando um evento ou condição específica ocorrer. A função callback permite que o código seja mais flexível e reutilizável, permitindo que uma função personalize o comportamento de outra função. 
+Uma função callback é uma função passada como argumento para outra função,
+que será chamada de volta (ou executada) quando um evento ou condição específica ocorrer. 
+A função callback permite que o código seja mais flexível e reutilizável,
+permitindo que uma função personalize o comportamento de outra função. 
 */
