89758bed60d09bc0b8433323d47dd6f69c054bbd
Lucas Santos <lucassq_@hotmail.com>
2023-04-16 18:12:30 -0300

    usando contains

--- ManipulandoClass/script.js
+++ ManipulandoClass/script.js
@@ -1,15 +1,10 @@
-function verde(){
-    document.querySelector('#exemplo').classList.remove('vermelho')
-    document.querySelector('#exemplo').classList.remove('azul')
-    document.querySelector('#exemplo').classList.add('verde')
-}
-function vermelho(){
-    document.querySelector('#exemplo').classList.remove('verde')
-    document.querySelector('#exemplo').classList.remove('azul')
-    document.querySelector('#exemplo').classList.add('vermelho')
-}
-function azul(){
-    document.querySelector('#exemplo').classList.remove('vermelho')
-    document.querySelector('#exemplo').classList.remove('verde')
-    document.querySelector('#exemplo').classList.add('azul')
+function trocar(){
+    let cor = document.querySelector('button').classList.contains('preto');
+    if(cor){
+        document.querySelector('button').classList.remove('preto');
+        document.querySelector('button').classList.add('verde');
+    }else{
+        document.querySelector('button').classList.remove('verde');
+        document.querySelector('button').classList.add('preto');
+    }
 }
